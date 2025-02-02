c660488d22e03f70c7abae7ed1e938462d2a5e6a
xplor4r <147792338+xplor4r@users.noreply.github.com>
2025-02-02 09:59:14 +0530

    update lint errors

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -4,7 +4,7 @@ import Hero from "@/components/hero";
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
-import "./../app/app.css";
+
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";

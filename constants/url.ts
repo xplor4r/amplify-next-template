666e61db1f2e97e836627f56a03adc3983dae17c
xplor4r <147792338+xplor4r@users.noreply.github.com>
2025-02-04 21:30:31 +0530

    update changes

--- constants/url.ts
+++ constants/url.ts
@@ -1,15 +1,17 @@
 import { getRangeDateForFilter } from './date';
 
 const isProduction = process.env.NODE_ENV === 'production';
+console.log('isProduction >>', isProduction);
+const domain = process.env.NEXT_PUBLIC_PROJECT_URL;
 
-const domain = 'ddg8iicsr72an.amplifyapp.com/';
 const local = 'localhost:3000';
 const home = isProduction ? domain : local;
 
 import { views } from './table';
 export const url = {
-	serverApi: `${isProduction ? 'https://' : 'http://'}${home}`,
+	serverApi: `${isProduction ? domain : 'http://'}${home}`,
 };
+
 
 export const getApiUrl = (filterKey: string, apiPath: string, categories: string[] = [], isNotRange = false) => {
 	if (isNotRange) {
